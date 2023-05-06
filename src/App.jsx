d49f869a5d30537edad8806cf14903c9050a1bf0
teriTheDesigner <tere1109@stud.kea.dk>
2023-05-06 21:51:28 +0200

    useState and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import React, { Component } from "react";
+import Welcome from "./components/Welcome";
+import Support from "./components/Support";
+import ListCast from "./components/ListCast";
+
 function App() {
+  const name = "StarGazers";
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
-  )
+    <div className="container">
+      <article>
+        <hgroup>
+          <img src="images/group.svg" alt="stargazers_group" />
+          <Welcome name="StarGazers" />
+          <p>
+            Members of an <b>intergalactic alliance</b> paving the way for peace
+            and benevolence among all species. They are known for their
+            enthusiasm for science, for their love of fun, and their dedication
+            to education.
+          </p>
+          <ListCast />
+          <Support />
+        </hgroup>
+      </article>
+    </div>
+  );
 }
-export default App
+export default App;
